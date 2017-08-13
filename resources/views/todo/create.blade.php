37eeb5789360c97266bce42fd0a2f702aa1b4f6f
touhidasi <asifrhmn430@gmail.com>
2017-08-12 23:25:37 -0700

    ToDo list Task

---
+++ resources/views/todo/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+
+@section('body')
+
+<br>
+<div class="col-lg-4 col-lg-offset-4">
+<h1>Create new Item for ToDo List</h1>
+</div>
+
+@endsection
---
+++ resources/views/todo/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+
+
+@section('body')
+<br>
+<a href="todo/create" class="btn btn-info">Add New</a>
+
+<div class="col-lg-4 col-lg-offset-4">
+    <center><h1>ToDo List</h1></center>
+    <ul class="list-group">
+  <li class="list-group-item">
+   
+   This is first item
+  </li>
+  
+</ul>  
+</div>
+
+
+@endsection
