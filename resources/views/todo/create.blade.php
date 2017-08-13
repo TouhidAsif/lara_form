c02ceb4b923312324135d00619d25280929b20b0
touhidasi <asifrhmn430@gmail.com>
2017-08-13 00:23:01 -0700

    csrf added

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -7,19 +7,16 @@
 <a href="/todo" class="btn btn-info">Back</a>
 <div class="col-lg-4 col-lg-offset-4">
 <h1>Create new Item for ToDo List</h1>
-<form class="form-horizontal">
+{{csrf_field()}}
+<form class="form-horizontal" action="/todo" method="post">
   <fieldset>
-    
     <div class="form-group">
-       
       <div class="col-lg-10">
         <textarea class="form-control" rows="5" id="textArea"></textarea>
         <button type="submit" class="btn btn-success">Submit</button>
  
       </div>
     </div>
-    
- 
 
       </div>
      
